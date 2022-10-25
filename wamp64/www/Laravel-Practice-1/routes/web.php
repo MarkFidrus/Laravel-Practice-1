4a533c2e860e43b36085f0d4e443d9e8d82ca55a
Fidrus Márk <markfidrus@gmail.com>
2022-10-25 17:45:13 +0200

    Several routes has been created for photo, gallery and profile.

--- wamp64/www/Laravel-Practice-1/routes/web.php
+++ wamp64/www/Laravel-Practice-1/routes/web.php
@@ -13,6 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', '\App\Http\Controllers\HomeController@index');
+
+Route::resource('gallery', '\App\Http\Controllers\GalleryController');
+Route::get('/gallery/create', '\App\Http\Controllers\GalleryController@create');
+Route::post('/gallery/create', '\App\Http\Controllers\GalleryController@store');
+
+Route::get('/gallery/show/{id}', '\App\Http\Controllers\GalleryController@show');
+
+Route::get('/gallery/edit/{id}', '\App\Http\Controllers\GalleryController@edit');
+Route::post('/gallery/edit/{id}', '\App\Http\Controllers\GalleryController@set');
+
+
+Route::resource('photo', '\App\Http\Controllers\PhotoController');
+Route::get('/photo/upload', '\App\Http\Controllers\PhotoController@upload');
+Route::post('/photo/upload', '\App\Http\Controllers\PhotoController@store');
+
+Route::get('/photo/show/{id}', '\App\Http\Controllers\GalleryController@show');
+
+Route::get('/photo/edit/{id}', '\App\Http\Controllers\GalleryController@edit');
+Route::post('/photo/edit/{id}', '\App\Http\Controllers\GalleryController@set');
+
+
+Route::resource('profile', '\App\Http\Controllers\ProfileController');
+Route::get('/profile/show/{id}', '\App\Http\Controllers\PhotoController@show');
+
