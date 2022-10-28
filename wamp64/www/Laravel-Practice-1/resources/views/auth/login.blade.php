4ffd01713c4779317faec594442875da75b7f7a9
Fidrus Márk <markfidrus@gmail.com>
2022-10-28 23:21:30 +0200

    Login, footer_short.blad.php and login.less has been finished.

--- wamp64/www/Laravel-Practice-1/resources/views/auth/login.blade.php
+++ wamp64/www/Laravel-Practice-1/resources/views/auth/login.blade.php
@@ -12,37 +12,41 @@
         <form method="POST" action="{{ route('login') }}">
             @csrf
             <div class="login__container-content-header">
-                <h1 class="login__container-content-header-title">{{ __('Login') }}</h1>
+                <h1 class="login__container-content-header-title">{{ __('LOGIN') }}</h1>
             </div>
             <div class="login__container-content-body">
                 <div class="login__container-content-body-email__field">
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <input id="email" type="email" class="login__container-content-body-email__field-input" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Your email address">
+                    <i class="login__container-content-body-email__field-icon fa-solid fa-envelope"></i>
                     <label class="login__container-content-body-email__field-label" for="email">{{ __('Email Address') }}</label>
                 </div>
                 <div class="login__container-content-body-password__field">
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                    <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                    <input id="password" type="password" class="login__container-content-body-password__field-input" name="password" required autocomplete="current-password" placeholder="Your password">
+                    <i class="login__container-content-body-password__field-icon fa-solid fa-lock"></i>
+                    <label for="password" class="login__container-content-body-password__field-label">{{ __('Password') }}</label>
                 </div>
-                <div class="login__container-content-body-other">
-                    <div class="login__container-content-body-other-remember__field">
-                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                        <label class="form-check-label" for="remember">
-                            {{ __('Remember Me') }}
-                        </label>
-                    </div>
-                    <div class="login__container-content-body-other-password__reset__field">
-                        @if (Route::has('password.request'))
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
-                                {{ __('Forgot Your Password?') }}
-                            </a>
-                        @endif
-                    </div>
+                <div class="login__container-content-body-btn__field">
+                    <button type="submit" class="login__container-content-body-btn__field-btn">{{ __('Login') }}</button>
                 </div>
             </div>
             <div class="login__container-content-footer">
-                <div class="login__container-content-footer-btn__field">
-                    <button type="submit" class="btn btn-primary">{{ __('Login') }}</button>
+                <div class="login__container-content-footer-row__1">
+                    <div class="login__container-content-footer-row__1-remember__field">
+                        <input class="login__container-content-footer-row__1-remember__field-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <label class="login__container-content-footer-row__1-remember__field-label" for="remember">{{ __('Remember Me') }}</label></div>
+                    <div class="login__container-content-footer-row__1-reset__password__field">
+                        @if (Route::has('password.request'))
+                            <a class="login__container-content-footer-row__1-reset__password__field-link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Password ?') }}
+                            </a>
+                        @endif
+                    </div>
+
+                </div>
+                <div class="login__container-content-footer-row__2">
+                    <p class="login__container-content-footer-row__2-text">{{__("Don't have an account ?")}}</p>
+                    <a class="login__container-content-footer-row__2-link" href="/register">{{ __('Create account') }}<i class="login__container-content-footer-row__2-link-icon fa-solid fa-hand-point-left"></i></a>
+
                 </div>
             </div>
         </form>
--- wamp64/www/Laravel-Practice-1/resources/views/includes/footer_short.blade.php
+++ wamp64/www/Laravel-Practice-1/resources/views/includes/footer_short.blade.php
@@ -1,8 +1,8 @@
 @section('footer_short')
     <footer class="footer__short">
-        <a class="footer__short-terms" href="/general_terms_and_conditions"><i class="footer__short-terms-icon fa-solid fa-file-circle-question"></i>General terms and conditions</a>
-        <p class="footer__short-text">All rights reserved &copy; Laravel Practice Project - 2022.10.28.</p>
-        <a class="footer__short-about__us" href="/about_us"><i class="footer__short-about__us-icon fa-solid fa-circle-question"></i>About us</a>
-        <a class="footer__short-contact__us" href="/contact_us"><i class="footer__short-contact__us-icon fa-solid fa-message"></i>Contact us</a>
+        <a class="footer__short-terms" href="/general_terms_and_conditions"><i class="footer__short-terms-icon fa-solid fa-file-circle-question"></i>{{__('Terms Of Use')}}</a>
+        <p class="footer__short-text">&copy;{{__(' 2022 - All Rights Reserved | Created by Mark Fidrus')}}</p>
+        <a class="footer__short-about__us" href="/about_us"><i class="footer__short-about__us-icon fa-solid fa-circle-question"></i>{{__('About us')}}</a>
+        <a class="footer__short-contact__us" href="/contact_us"><i class="footer__short-contact__us-icon fa-solid fa-message"></i>{{__('Contact us')}}</a>
     </footer>
 @endsection
