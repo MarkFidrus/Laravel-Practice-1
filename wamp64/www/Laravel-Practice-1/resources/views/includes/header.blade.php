f598fd73515cd182ec9a2d4a7d9fb050964ded4c
Fidrus Márk <markfidrus@gmail.com>
2022-10-30 16:51:28 +0100

    Header.blade.php, header.less, footer.less has been refactored.

--- wamp64/www/Laravel-Practice-1/resources/views/includes/header.blade.php
+++ wamp64/www/Laravel-Practice-1/resources/views/includes/header.blade.php
@@ -3,11 +3,11 @@
         <nav class="header-navigation">
             <a class="header-navigation-brand" href="/"><i class="header-navigation-brand-icon fa-solid fa-fish"></i><p class="header-navigation-brand-text">Fish site</p></a>
 
-            <a class="header-navigation-menu-item"id="galleriesHeaderBtn" href="/galleries"><i class="header-navigation-menu-item-icon fa-solid fa-image"></i>Galleries</a>
+            <a class="header-navigation-item" id="galleriesHeaderBtn" href="/galleries"><i class="header-navigation-item-icon fa-solid fa-image"></i><p class="header-navigation-item-text">Galleries</p></a>
             @if(\Illuminate\Support\Facades\Auth::guest())
-                <a class="header-navigation-menu-item" id="loginHeaderBtn" href="/login"><i class="header-navigation-menu-item-icon fa-solid fa-right-to-bracket"></i>Login</a>
-                <a class="header-navigation-menu-item" id="registerHeaderBtn" href="/register"><i class="header-navigation-menu-item-icon fa-solid fa-user-plus"></i>Register</a>
-            @else<a class="header-navigation-menu-item" id="logoutHeaderBtn" href="/logout"><i class="header-navigation-menu-item-icon fa-solid fa-right-from-bracket"></i>Logout</a>
+                <a class="header-navigation-item" id="loginHeaderBtn" href="/login"><i class="header-navigation-item-icon fa-solid fa-right-to-bracket"></i><p class="header-navigation-item-text">Login</p></a>
+                <a class="header-navigation-item" id="registerHeaderBtn" href="/register"><i class="header-navigation-item-icon fa-solid fa-user-plus"></i><p class="header-navigation-item-text">Register</p></a>
+            @else<a class="header-navigation-item" id="logoutHeaderBtn" href="/logout"><i class="header-navigation-item-icon fa-solid fa-right-from-bracket"></i><p class="header-navigation-item-text">Logout</p></a>
             @endif
         </nav>
     </header>
