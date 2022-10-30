a8e9c172dfbfe1d693d7e65e6e7f718446e67a3d
Fidrus Márk <markfidrus@gmail.com>
2022-10-30 02:57:01 +0200

    Alert_message, register, login.less has been refactored. New message has been added to lang/en/auth.php. Alert_message.blade.php has been refactored.

--- wamp64/www/Laravel-Practice-1/resources/views/includes/alert_message.blade.php
+++ wamp64/www/Laravel-Practice-1/resources/views/includes/alert_message.blade.php
@@ -1,10 +1,14 @@
 @section('alert_message')
-    @if(Session::has('message_text') && Session::has('message_type'))
-        <div class="alert_message_container {{ Session::get('message_type') }}" id="alert_message">
+    @if(Session::has('message_text'))
+        <div class="alert_message_container success" id="alert_message">
             <p class="alert_message_container-message">{{ Session::get('message_text') }}</p>
         </div>
     @endif
-    @error('email')
-        <p>{{ $message }}</p>
-    @enderror
+    @if($errors->any())
+        @foreach($errors->all() as $error)
+            <div class="alert_message_container error" id="alert_message">
+                <p class="alert_message_container-message">{{ $error }}</p>
+            </div>
+        @endforeach
+    @endif
 @endsection
