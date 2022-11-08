b3e74b0c45863a5046951ad61aec26d709e7d43b
Fidrus Márk <markfidrus@gmail.com>
2022-11-08 01:15:10 +0100

    Profile_picture column has been added to users table.

--- wamp64/www/Laravel-Practice-1/database/migrations/2014_10_12_000000_create_users_table.php
+++ wamp64/www/Laravel-Practice-1/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('profile_picture')->default('no_profile_picture.png');
             $table->string('password');
             $table->tinyInteger('is_sanctioned')->default(0);
             $table->tinyInteger('is_admin')->default(0);
