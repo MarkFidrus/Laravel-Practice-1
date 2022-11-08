fbea0487e6582052bfd175ff98c1f4eaa509c12c
Fidrus Márk <markfidrus@gmail.com>
2022-11-08 01:09:15 +0100

    Dark style has been added to register.less, login.less, home.less, header.less has been refactored. Header.blade.php has been reafactored. Added auth middleware to some routes.

--- wamp64/www/Laravel-Practice-1/routes/web.php
+++ wamp64/www/Laravel-Practice-1/routes/web.php
@@ -15,41 +15,68 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['middleware' => 'web'], function () {
-    Route::get('/', '\App\Http\Controllers\HomeController@index')->name('home');
+    Route::get('/', '\App\Http\Controllers\HomeController@index')
+        ->name('home');
 
     Route::resource('gallery', '\App\Http\Controllers\GalleryController');
 
-    Route::get('/galleries', '\App\Http\Controllers\GalleryController@index')->name('galleries');
+    Route::get('/galleries', '\App\Http\Controllers\GalleryController@index')
+        ->name('galleries')
+        ->middleware('auth');
 
-    Route::get('/gallery/create', '\App\Http\Controllers\GalleryController@create')->name('create_gallery');
+    Route::get('/gallery/create', '\App\Http\Controllers\GalleryController@create')
+        ->name('create_gallery');
     Route::post('/gallery/create', '\App\Http\Controllers\GalleryController@store');
 
-    Route::get('/gallery/show/{id}', '\App\Http\Controllers\GalleryController@show')->name('show_gallery');
+    Route::get('/gallery/show/{id}', '\App\Http\Controllers\GalleryController@show')
+        ->name('show_gallery');
 
-    Route::get('/gallery/edit/{id}', '\App\Http\Controllers\GalleryController@edit')->name('edit_gallery');
+    Route::get('/gallery/edit/{id}', '\App\Http\Controllers\GalleryController@edit')
+        ->name('edit_gallery');
     Route::post('/gallery/edit/{id}', '\App\Http\Controllers\GalleryController@set');
 
 
     Route::resource('photo', '\App\Http\Controllers\PhotoController');
-    Route::get('/photo/upload', '\App\Http\Controllers\PhotoController@upload')->name('upload_photo');
+    Route::get('/photo/upload', '\App\Http\Controllers\PhotoController@upload')
+        ->name('upload_photo');
     Route::post('/photo/upload', '\App\Http\Controllers\PhotoController@store');
 
-    Route::get('/photo/show/{id}', '\App\Http\Controllers\GalleryController@show')->name('show_photo');
+    Route::get('/photo/show/{id}', '\App\Http\Controllers\GalleryController@show')
+        ->name('show_photo');
 
-    Route::get('/photo/edit/{id}', '\App\Http\Controllers\GalleryController@edit')->name('edit_photo');
+    Route::get('/photo/edit/{id}', '\App\Http\Controllers\GalleryController@edit')
+        ->name('edit_photo');
     Route::post('/photo/edit/{id}', '\App\Http\Controllers\GalleryController@set');
 
 
     Route::resource('profile', '\App\Http\Controllers\ProfileController');
-    Route::get('/profile/{id}', '\App\Http\Controllers\PhotoController@show')->name('profile');
+    Route::get('/profile/{id}', '\App\Http\Controllers\PhotoController@show')
+        ->name('profile')
+        ->middleware('auth');
 
-    Route::get('/contact_us', '\App\Http\Controllers\ContactController@contact')->name('contact');
+    Route::resource('games', '\App\Http\Controllers\GameController');
+    Route::get('/games', '\App\Http\Controllers\GameController@index')
+        ->name('games')
+        ->middleware('auth');
+
+
+    Route::resource('apps', '\App\Http\Controllers\OtherAppController');
+    Route::get('/apps', '\App\Http\Controllers\OtherAppController@index')
+        ->name('apps')
+        ->middleware('auth');
+
+
+    Route::get('/contact_us', '\App\Http\Controllers\ContactController@contact')
+        ->name('contact');
     Route::post('/contact_us', '\App\Http\Controllers\ContactController@send');
 
-    Route::get('/messages', '\App\Http\Controllers\PhotoController@show')->name('messages');
-    Route::get('/messages/show/{id}', '\App\Http\Controllers\PhotoController@show')->name('show_messages');
+    Route::get('/messages', '\App\Http\Controllers\PhotoController@show')
+        ->name('messages');
+    Route::get('/messages/show/{id}', '\App\Http\Controllers\PhotoController@show')
+        ->name('show_messages');
 
-    Route::get('/about_us', '\App\Http\Controllers\HomeController@about')->name('about_us');
+    Route::get('/about_us', '\App\Http\Controllers\HomeController@about')
+        ->name('about_us');
 
     Auth::routes();
 
